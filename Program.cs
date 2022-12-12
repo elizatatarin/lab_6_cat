8ef022355115a9533cff0be70f741b6f943a2f88
eliza <eliza@DESKTOP-HLLIIPJ>
2022-12-12 19:32:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_6_cat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat("Jack", Gender.Male);
+
+            cat1.Jump();
+            cat1.Jump();
+            cat1.Jump();
+            cat1.Jump();
+            cat1.Jump();
+            cat1.Jump();
+            cat1.Jump();
+            cat1.CheckEnergy();
+            cat1.Jump();
+            cat1.Jump();
+            cat1.Jump();
+            cat1.CheckEnergy();
+            cat1.Sleep();
+            cat1.Sleep();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
